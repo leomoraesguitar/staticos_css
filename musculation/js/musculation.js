d7b58ea8868bf6713518aa39766dcc238e76c69d
leomoraesguitar <123950901+leomoraesguitar@users.noreply.github.com>
2025-05-02 14:19:59 -0300

    Atualizando arquivos estáticos

--- musculation/js/musculation.js
+++ musculation/js/musculation.js
@@ -193,8 +193,11 @@ document.addEventListener("DOMContentLoaded", async () => {
         // console.log('grupos', grupos )
         let grupoAtual = grupos[grupoIndex];
         let exercicioAtual = listas[grupoAtual][exercicioIndex];
+
+        //set os valores das variaves que serão enviadas para savalr os comentarios
         dia_comentario.value = grupoAtual || "";
         exercicio_comentario.value = exercicioAtual || "";
+
         // Adiciona event listeners para pesoInicial e pesoFinal
         const pesoInicialInput = document.getElementById("pesoInicial");
         const pesoFinalInput = document.getElementById("pesoFinal");
